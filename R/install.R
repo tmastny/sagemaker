296098916c22f9da828aaef8cdf3eaab43c56454
Timothy Mastny <tim.mastny@gmail.com>
2019-11-23 11:14:31 -0600

    also install awscli

--- R/install.R
+++ R/install.R
@@ -1,7 +1,6 @@
 #' @export
 install_sagemaker <- function(method = "auto", conda = "auto") {
   reticulate::py_install(
-    c("boto3", "sagemaker"), method = method, conda = conda
+    c("boto3", "sagemaker", "awscli"), method = method, conda = conda
   )
 }
-
