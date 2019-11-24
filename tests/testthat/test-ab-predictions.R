0e9d3e3b49a712f8fa13f23c54b47d873dec2f52
Timothy Mastny <tim.mastny@gmail.com>
2019-11-24 11:29:49 -0600

    rename for run order

--- tests/testthat/test-ab-predictions.R
+++ tests/testthat/test-ab-predictions.R
@@ -1,4 +1,6 @@
 test_that("batch predictions work", {
+  skip_if_not(s3_bucket() == "sagemaker-us-east-2-495577990003")
+
   model <- sagemaker_attach_tuner("xgboost-191114-2052")
   batch_predict(
     model,
