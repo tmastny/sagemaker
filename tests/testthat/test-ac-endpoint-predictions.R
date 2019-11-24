ce52f2cf89f99eedf4e3ec5468c18ba3175e728d
Timothy Mastny <tim.mastny@gmail.com>
2019-11-24 12:20:28 -0600

    testing endpoint predictions

--- tests/testthat/test-ac-endpoint-predictions.R
+++ tests/testthat/test-ac-endpoint-predictions.R
@@ -12,8 +12,12 @@ test_that("endpoint deploys", {
 })
 
 test_that("endpoint predictions work", {
+  on.exit(sagemaker_delete_endpoint(model))
+
   model <- sagemaker_attach_tuner("xgboost-191114-2052")
   predictions <- predict(model, sagemaker::abalone[1:100, -1])
 
-  expect_equal(predictions, sagemaker::abalone_pred)
+  df_pred <- tibble::enframe(predictions, NULL, "X1")
+
+  expect_equal(df_pred, sagemaker::abalone_pred)
 })
