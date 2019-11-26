807cadfbc3e7ef4a5d18cd1c9bf9dfb25301ed7c
Timothy Mastny <tim.mastny@gmail.com>
2019-11-26 15:43:42 -0600

    separate tuning code

--- R/tuning.R
+++ R/tuning.R
@@ -22,3 +22,93 @@ sagemaker_continuous <- function(min, max, scaling = "Auto") {
 sagemaker_categorical <- function(values) {
   sagemaker$tuner$CategoricalParameter(values)
 }
+
+
+# resamples resamples rsplit object, or a python dictionary with
+#           train/test keys pointing to sagemaker$s3_input paths
+#' @export
+sagemaker_hyperparameter_tuner <- function(
+  estimator,
+  split,
+  hyperparameter_ranges,
+  strategy = "Random",
+  max_jobs = 10L,
+  max_parallel_jobs = 2L,
+  early_stopping_type = "Auto",
+  ...
+) {
+
+  max_jobs <- as.integer(max_jobs)
+  max_parallel_jobs <- as.integer(max_parallel_jobs)
+
+  objective_metric <- objective_metric_name(estimator$hyperparam_dict$eval_metric)
+
+  tuner <- sagemaker$tuner$HyperparameterTuner(
+    estimator = estimator,
+    objective_metric_name = objective_metric,
+    objective_type = objective_metric_type(objective_metric),
+    hyperparameter_ranges = hyperparameter_ranges,
+    strategy = strategy,
+    max_jobs = max_jobs,
+    max_parallel_jobs = max_parallel_jobs,
+    early_stopping_type = early_stopping_type,
+    ...
+  )
+
+  # TODO: if given a rsplit object, need to upload it s3
+
+  stopifnot("train" %in% names(split))
+  stopifnot("validation" %in% names(split))
+
+  split_dict <- reticulate::dict(
+    train = sagemaker$s3_input(split$train, content_type = "text/csv"),
+    validation = sagemaker$s3_input(split$validation, content_type = "text/csv")
+  )
+
+  tuner$fit(split_dict)
+  tuner$wait()
+
+  sagemaker_attach_tuner(tuner$latest_tuning_job$job_name)
+}
+
+
+#' @export
+sagemaker_attach_tuner <- function(tuning_job_name) {
+
+  tuner_df <- sagemaker_tuning_job_logs(tuning_job_name)
+
+  model_name <- tuner_df %>%
+    dplyr::filter(final_objective_value == min(final_objective_value)) %>%
+    dplyr::pull(training_job_name)
+
+  tuner <- sagemaker$tuner$HyperparameterTuner$attach(tuning_job_name)
+
+  tuning_parameter_names <- tuner$hyperparameter_ranges() %>%
+    purrr::discard(purrr::is_empty) %>%
+    purrr::flatten() %>%
+    purrr::map_chr(purrr::pluck, "Name") %>%
+    purrr::set_names(NULL)
+
+  best_tune <- tuner_df %>%
+    dplyr::filter(training_job_name == model_name) %>%
+    dplyr::select(dplyr::one_of(tuning_parameter_names))
+
+  best_eval_metric <- tuner_df %>%
+    dplyr::filter(training_job_name == model_name) %>%
+    dplyr::pull(final_objective_value)
+
+  model_obj <- list(
+    model_name = model_name,
+    tuning_job_name = tuning_job_name,
+    eval_metric = tuner$estimator$hyperparam_dict$eval_metric,
+    strategy = tuner$strategy,
+    best_eval_metric = best_eval_metric,
+    best_tune = best_tune,
+    metrics = tuner_df
+  )
+
+  # TODO: Constructors, validators: https://adv-r.hadley.nz/s3.html#s3-constructor
+
+  class(model_obj) <- "sagemaker"
+  model_obj
+}
